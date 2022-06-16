f2554ef3a92a38e42d744cda35bd83e40374ff02
Benoit Cote <cotebenoit8@gmail.com>
2022-06-16 11:18:28 -0700

    BC: Added tests directory with one pass and one fail test

---
+++ tests/test_reading.py
@@ -0,0 +1,20 @@
+# Tests for the reading process
+# Created by: Benoit Cote (June, 2022)
+
+# Import Interface tools
+#from interface import read_data_file
+
+# TestReading class
+# =================
+class TestReading:
+    """Class that tests the reading functionalities. """
+
+    # Instantiate the reading scripts
+#    rdf = read_data_file.read_data_file(root_path="./data/")
+
+    def test_ttt(self):
+        assert True
+
+    def test_aaa(self):
+        assert False
+    
