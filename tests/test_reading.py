75fb13f108e16260b7b50bd2234775599aef5627
Benoit Cote <cotebenoit8@gmail.com>
2022-06-16 13:08:29 -0700

    BC: Added first test for reading functionalities

--- tests/test_reading.py
+++ tests/test_reading.py
@@ -9,9 +9,39 @@ from Interface import read_data_file
 class TestReading:
     """Class that tests the reading functionalities. """
 
-    # Instantiate the reading scripts
-#    rdf = read_data_file.read_data_file(root_path="./data/")
+    # Instantiate the reading scripts and setting root folder
+    rdf = read_data_file.read_data_file(root_path="./tests/data/")
 
-    def test_ttt(self):
-        assert True
-    
+    # Compare values
+    # ==============
+    def compare_values(self, data, test_case):
+        """Compare raw values with the ones read from a file. """
+
+        # Test each case
+        for i_entry, key, value in test_case:
+            assert data[key][i_entry] == value
+
+
+    # Test file #1
+    # ============
+    def test_file_1(self):
+
+        # Read data file
+        d = self.rdf.read_file("file_1.txt", "file_1_structure.txt")
+
+        # Fill the raw values to be tested
+        test_case = []
+        test_case.append([0,"element","H"])
+        test_case.append([1,"element","He"])
+        test_case.append([2,"element","Li"])
+        test_case.append([0,"Z",1])
+        test_case.append([1,"Z",2])
+        test_case.append([2,"Z",3])
+        test_case.append([0,"value",0.001])
+        test_case.append([1,"value",0.002])
+        test_case.append([2,"value",0.003])
+
+        # Compare raw values with the ones read from file
+        self.compare_values(d.data, test_case)
+
+
